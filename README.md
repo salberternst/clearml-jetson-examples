e71f2bbec77c417dc1bc29b890a82a94bdb1bf37
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2025-03-13 14:38:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clearml jetson examples
+
+The `nano` folder contains examples for the Jetson Nano using a special ClearML queue. Only pytorch 1.10 and tensorflow 2.7 are supported.
---
+++ nano/pytorch-1.10/training.py
@@ -0,0 +1,54 @@
+from clearml import Task
+
+task = Task.init(project_name='jetson-nano-examples',task_name='Simple Training Pytorch')
+
+task.add_requirements('torch', '1.10.0')
+task.add_requirements('torchaudio', '0.10.0+d2634d8')
+task.add_requirements('torchvision', '0.11.0a0+fa347eb')
+task.add_requirements('numpy', '1.19.5')
+task.add_requirements('Pillow', '9.0.1')
+
+task.set_base_docker(docker_image='nvcr.io/nvidia/l4t-pytorch:r32.7.1-pth1.10-py3')
+task.execute_remotely(
+    queue_name='jetson-nano',
+    exit_process=True
+)
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+x_train = torch.FloatTensor(np.random.random((100, 10)))
+y_train = torch.LongTensor(np.random.randint(2, size=(100,)))
+
+class SimpleNN(nn.Module):
+    def __init__(self):
+        super(SimpleNN, self).__init__()
+        self.layer1 = nn.Linear(10, 16)
+        self.relu = nn.ReLU()
+        self.layer2 = nn.Linear(16, 1)
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, x):
+        x = self.relu(self.layer1(x))
+        x = self.sigmoid(self.layer2(x))
+        return x
+
+model = SimpleNN()
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters())
+
+batch_size = 16
+epochs = 3
+
+for epoch in range(epochs):
+    for i in range(0, len(x_train), batch_size):
+        batch_x = x_train[i:i+batch_size]
+        batch_y = y_train[i:i+batch_size].float().unsqueeze(1)
+        outputs = model(batch_x)
+        loss = criterion(outputs, batch_y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
